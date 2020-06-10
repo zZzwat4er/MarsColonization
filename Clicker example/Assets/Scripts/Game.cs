b37c6a47d115b61d41802a4441e5530a7fbce33e
maksktl <matantsev0@gmail.com>
2020-06-10 15:51:08 +0600

    Just example of clicker

---
+++ Clicker example/Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    private int score = 0;
+    public Text scoreText;
+
+	public void OnClick()
+    {
+        ++score;
+        scoreText.text = score + "$"; 
+    }
+}
