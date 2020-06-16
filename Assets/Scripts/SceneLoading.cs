565a5fbd7a3dabb73eaa244f765370de5f3848da
maksktl <matantsev0@gmail.com>
2020-06-17 02:53:37 +0600

    Добавлена сцена загрузки

---
+++ Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneLoading : MonoBehaviour
+{
+    [Header("Остальный объекты")]
+    public Image ProgressImg;
+    public Text ProgressTxt;
+    
+    [Header("Загружаемая сцена")]
+    public int sceneId;
+    void Start()
+    {
+        StartCoroutine(AsyncLoad());
+    }
+
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        while(!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            ProgressImg.fillAmount = progress;
+            ProgressTxt.text = Convert.ToInt32(progress * 100) + "%";
+            yield return null;
+        }
+        
+    }
+}
