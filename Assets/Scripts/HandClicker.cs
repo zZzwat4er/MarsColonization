1f6fc3dcf89aed82c4f9a2bc305f3fe1e698f54d
maksktl <matantsev0@gmail.com>
2020-07-03 12:52:34 +0300

    Пофиксен звук, добавлен апгрейд кликера

--- Assets/Scripts/HandClicker.cs
+++ Assets/Scripts/HandClicker.cs
@@ -10,16 +10,18 @@ namespace Game_Classes
     {
         public BigInteger[] costs =
         {
+            15,
             1000, 
             BigInteger.Parse("1000000000000000000000000000000000")
         };//цены на апгрейд хэндкликера
 
-        public string[] names = {"New Tricks", "Endgame"};//их названя
+        public string[] names = {"MO' Money", "New Tricks", "Endgame"};//их названя
 
-        public int[] multipliers = {10,  100};//множитель для увеличения цены
+        public float[] multipliers = {1.5f, 10,  50};//множитель для увеличения цены
         
-        public int[] specCoef={0,  0};//специальный коэфициент для формулы
-        public int coef; // коэфициент для формулы
+        public int[] specCoef={0, 0,  0};//специальный коэфициент для формулы
+        public int coef; //коэфициент для формулы
+        public int amt;
         public BigInteger income; // доход с одного клика
         public BigInteger base_income;//базовый доход (по идее не нужен, но в формуле есть)
         public Building[] _buildings; //ссылки на здания
@@ -32,7 +34,8 @@ namespace Game_Classes
             _buildings = buildings;
             base_income = 1;
             income = 1;
-            
+            amt = 1;
+
         }
 
         public BigInteger getNextIncome(int index)
@@ -40,16 +43,16 @@ namespace Game_Classes
             //здесь считаем всё по формуле данной в гуглдоке
             
             BigInteger res = 0;
-
+            BigInteger lvls = 0;
             for (int i = 0; i < 14; ++i)
             {
                 // Debug.Log(i + " " + specCoef[1]);
-                res += (specCoef[1] + (index == 1 ? 1 : 0)) * (_buildings[i].Lvl / 10);
+                // res += (specCoef[2] + (index == 2 ? 1 : 0)) * (_buildings[i].Lvl / 10);
+                lvls += _buildings[i].Lvl;
             }
-
-            res += (coef + (index == 0 ? 1 : 0) + res);
+            
+            res = (coef + (index == 1 ? 1 : 0) + (specCoef[2] + (index == 2? 1 :0))*lvls/10)*(amt + (index == 0? 1:0));
             return res;
-
         }
 
         public BigInteger Up(int index, BigInteger money)
@@ -58,11 +61,14 @@ namespace Game_Classes
             if (money >= costs[index]) //если денег достаточно, то делаем по формулам
             {
                 income = getNextIncome(index);
-                if (index == 0) coef++;
+                if (index == 1) coef++;
+                if (index == 0) amt++;
                 specCoef[index]++;
-                costs[index] *= multipliers[index];
+                BigInteger  ret = BigInteger.Parse(costs[index].ToString());
+                costs[index] *= (int)(multipliers[index]*10);
+                costs[index] /= 10;
                 // Debug.Log("Куплено " + income);
-                return costs[index]/multipliers[index];//возвращаем сколько мы потратили, если хватило денег
+                return ret;//возвращаем сколько мы потратили, если хватило денег
             }
             // Debug.Log("Не куплено");
             return 0;//возвращаем 0 если не хватило денег
