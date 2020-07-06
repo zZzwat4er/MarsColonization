c1249f0a1323c8bd9a2cb3fcad2a825aa57236b9
maksktl <matantsev0@gmail.com>
2020-07-06 11:50:09 +0300

    Ползунок громкости и Реклама

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Advertisements;
+public class Ads : MonoBehaviour
+{
+
+    private string gameId = "3697725", type = "video";
+    private bool testMode = true;
+    [SerializeField] private GameLogic _;
+    public void watchAd()
+    {
+        Advertisement.Initialize(gameId, testMode);
+
+        while (!Advertisement.IsReady(type))
+        {
+            Thread.Sleep(1000);
+        }
+        Advertisement.Show(type);
+        _.GPrime += 1;
+    }
+}
--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -9,31 +9,41 @@ public class AudioSettings : MonoBehaviour
     [Header("Объекты со звуком")] [SerializeField]
     private AudioSource backgroundMusic, handClickSound, buySound;
 
-    [Header("Кнопки в настройках")] [SerializeField]
-    private Button musicButton, soundButton;
+    [Header("Текст в настройках")] [SerializeField]
+    private Text musicButton, soundButton;
 
-    public void switchMusic()
+    [Header("Ползунки громкости")] [SerializeField]
+    private Slider music, sound;
+
+    
+    
+    public void switchMusic(float value)
     {
         //функция выключает или включает музыку на бэкграунде
-        switcher(backgroundMusic);
-        musicButton.GetComponentInChildren<Text>().text =
-            (backgroundMusic.enabled == true ? "Music: on" : "Music: off");
+        switcher(backgroundMusic, value);
+        musicButton.text =
+            "Music: " + (int) (value * 100) + "%";
     }
 
-    void switcher(AudioSource src)
+    void switcher(AudioSource src, float value)
     {
         //переключает состояние аудио
-        src.enabled = !src.enabled;
+        src.volume = value;
         Debug.Log("Switched");
     }
 
-    public void switchSound()
+    public void switchSound( float value)
     {
         //функция включает или выключает звуки
-        switcher(buySound);
-        switcher(handClickSound);
-        soundButton.GetComponentInChildren<Text>().text =
-            (buySound.enabled == true ? "Sound: on" : "Sound: off");
+        switcher(buySound, value);
+        switcher(handClickSound, value);
+        soundButton.text =
+           "Sound: "+ (int) (value * 100) + "%";
     }
-    
+
+    private void Update()
+    {
+        switchMusic(music.value);
+        switchSound(sound.value);
+    }
 }
